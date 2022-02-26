0af26081336559b6ff5c473f5cd64c060abce65f
VadimIgnatov <vignatov@my.yorku.ca>
2022-02-25 22:09:22 -0500

    UPDATE 3

--- src/App.js
+++ src/App.js
@@ -18,6 +18,13 @@ function App() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify (todos))
   }, [todos])
 
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo=> todo.id == id)
+    todo.complete =!todo.complete
+    setTodos(newTodos)
+  }
+
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
@@ -26,12 +33,18 @@ function App() {
     } )
     todoNameRef.current.value = null
   }
+
+  function handleClearTodos(){
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-    <TodoList todos={todos}  />
+    <TodoList todos={todos} toggleTodo={toggleTodo} />
     <input ref={todoNameRef} type="text" />
     <button onClick={handleAddTodo}>Add ToDo</button>
-    <button>Clear List</button>
+    <button onClick={handleClearTodos}> Clear List</button>
     </>
   )
 }
